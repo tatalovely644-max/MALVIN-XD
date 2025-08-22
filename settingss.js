b8d0ef66d11ce4305416307ef19af11e99c962ae
tatalovely644-max <tatalovely644@gmail.com>
2025-08-22 14:30:35 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,9 @@
 
 module.exports = {
   SESSION_ID: "malvin~Z6FWWRyL#HO1LfjuN_IWQmyFJKjsA4KUgCBtApVhFX5atAHF_s64",  // add your Session Id here
+ 
+MODE: process.env.MODE || "public",
+// make bot public-private-inbox-group 
   
   OWNER_NUMBER: "263776369078", // put your phone number here
   
