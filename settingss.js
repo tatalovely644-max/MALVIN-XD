a79b5c085bcf7508b55c550832f8bf6b512b8e6a
tatalovely644-max <tatalovely644@gmail.com>
2025-08-22 18:11:42 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~Z6FWWRyL#HO1LfjuN_IWQmyFJKjsA4KUgCBtApVhFX5atAHF_s64",  // add your Session Id here
+  SESSION_ID: "malvin~xqFRzQ7L#qPLfBAh6Q-DG0KljnvsSON_YmgqBqbsSQNDirN2cFz8",  // add your Session Id here
  
 MODE: process.env.MODE || "public",
 // make bot public-private-inbox-group 
